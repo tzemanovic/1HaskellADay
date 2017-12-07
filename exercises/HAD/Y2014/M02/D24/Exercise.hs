e0e3224e5dfb2ad0d2047fc859477fda74ebb97c
Tomas Zemanovic <tzemanovic@gmail.com>
2017-12-07 12:46:02 +1100

    2014-02-24

--- exercises/HAD/Y2014/M02/D24/Exercise.hs
+++ exercises/HAD/Y2014/M02/D24/Exercise.hs
@@ -1,5 +1,7 @@
 module HAD.Y2014.M02.D24.Exercise where
 
+import Data.List
+
 -- | Filter a list, keeping an element only if it is equal to the next one.
 --
 -- Examples:
@@ -9,6 +11,15 @@ module HAD.Y2014.M02.D24.Exercise where
 -- []
 -- >>> filterByPair [1, 2, 2, 2, 3, 3, 4]
 -- [2,2,3]
+filterByPair :: Eq a => [a] -> [a]
+filterByPair xs = group xs >>= init
 
---filterByPair :: Find the most generic signature
-filterByPair = undefined
+filterByPair' :: Eq a => [a] -> [a]
+filterByPair' = fst .
+  foldr (\x (acc, lastX) ->
+           case lastX of
+             Just lastX' ->
+                (if x == lastX' then x : acc else acc, Just x)
+             _ ->
+               (acc, Just x)
+        ) ([], Nothing)
