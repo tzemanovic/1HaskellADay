318d56c2b1c4d72b5d255461cc30399f06b1869f
Tomas Zemanovic <tzemanovic@gmail.com>
2017-12-11 12:32:20 +1100

    2014-03-03

--- exercises/HAD/Y2014/M03/D03/Exercise.hs
+++ exercises/HAD/Y2014/M03/D03/Exercise.hs
@@ -1,5 +1,8 @@
 module HAD.Y2014.M03.D03.Exercise where
 
+import Data.List
+import Data.Ord
+
 -- | Sort a list of list of elements by the maximum of each list,
 -- in ascending order
 -- 
@@ -15,5 +18,5 @@ module HAD.Y2014.M03.D03.Exercise where
 -- sortByMax [[], [1,2]]
 -- should throw an execption: no max for empty list
 
--- sortByMax :: Find the most generic signature
-sortByMax = undefined
+sortByMax :: Ord a => [[a]] -> [[a]]
+sortByMax = sortBy (comparing maximum)
